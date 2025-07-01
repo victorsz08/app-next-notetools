b6f783e4b4aa601e705e2e8284708369c7e07e63
Victor Developer <victorszdeveloper@gmail.com>
2025-07-01 19:48:01 +0000

    change method jwt token

--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -41,7 +41,10 @@ export function LoginForm() {
         try {
             const response = await api.post('auth/login', data);
             if (response.status === 200) {
-                localStorage.setItem('nt.authtoken', response.data.token);
+                window.localStorage.setItem(
+                    'nt.authtoken',
+                    response.data.token
+                );
                 router.push('/dashboard');
             }
         } catch (error: any) {
--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -39,7 +39,7 @@ export function AuthContextProvider({
         queryFn: async () => {
             const { data, status } = await api.get('auth/session');
             if (status === 401) {
-                localStorage.removeItem('nt.authtoken');
+                window.localStorage.removeItem('nt.authtoken');
                 router.push('/auth/login');
             }
 
@@ -73,7 +73,7 @@ export const useAuth = () => {
     const queryClient = useQueryClient();
 
     const logout = async () => {
-        localStorage.removeItem('nt.authtoken');
+        window.localStorage.removeItem('nt.authtoken');
         queryClient.clear();
         router.push('/auth/login');
     };
