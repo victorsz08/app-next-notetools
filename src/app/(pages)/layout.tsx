321bb2953477a78fda382769ecb1f69f0c031278
Victor Developer <victorszdeveloper@gmail.com>
2025-07-01 22:47:20 +0000

    fix context

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -5,22 +5,35 @@ import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { Toaster } from '@/components/ui/sonner';
 import { AvatarProfile } from '@/components/avatar/avata-profile';
 import { BreadcrumbGroup } from '@/components/breadcrumb/breadcrumb';
+import { AuthContextProvider } from '@/context/auth-context';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                staleTime: 3600 * 5,
+            },
+        },
+    });
     return (
         <SidebarProvider>
-            <AppSidebar collapsible="icon" variant="sidebar" />
-            <main className="bg-background w-full">
-                <header className="bg-sidebar flex items-center justify-between w-full p-4 border-b border-b-muted-foreground/10">
-                    <div className="flex items-center gap-3">
-                        <SidebarTrigger />
-                        <BreadcrumbGroup />
-                    </div>
-                    <AvatarProfile />
-                </header>
-                {children}
-                <Toaster richColors />
-            </main>
+            <QueryClientProvider client={queryClient}>
+                <AuthContextProvider>
+                    <AppSidebar collapsible="icon" variant="sidebar" />
+                    <main className="bg-background w-full">
+                        <header className="bg-sidebar flex items-center justify-between w-full p-4 border-b border-b-muted-foreground/10">
+                            <div className="flex items-center gap-3">
+                                <SidebarTrigger />
+                                <BreadcrumbGroup />
+                            </div>
+                            <AvatarProfile />
+                        </header>
+                        {children}
+                        <Toaster richColors />
+                    </main>
+                </AuthContextProvider>
+            </QueryClientProvider>
         </SidebarProvider>
     );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
-import { AuthContextProvider } from "@/context/auth-context";
-import "./globals.css";
+import './globals.css';
 import { Inter, Poppins, Barlow } from 'next/font/google';
-import { Providers } from './providers';
 
 const poppins = Poppins({
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -28,9 +26,7 @@ export default function RootLayout({
             <body
                 className={`${poppins.className} ${inter.variable} ${barlow.variable} antialiased bg-background`}
             >
-                <Providers>
-                    <AuthContextProvider>{children}</AuthContextProvider>
-                </Providers>
+                {children}
             </body>
         </html>
     );
